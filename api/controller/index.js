4b5c895fad2182ab91136b8649f30bce41c9613d
LC1007 <tonyg7212@gmail.com>
2023-08-19 12:17:38 +0200

    api done

--- api/controller/index.js
+++ api/controller/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express()
 const { products } = require('../model')
+const bodyParser = require('body-parser')
 
 router.get('/products', (req, res) =>{
     products.fetchProducts(req, res)
+})
+
+router.get('/product/:skateID', (req, res) =>{
+    products.fetchProduct(req, res)
+})
+
+router.post('/products', bodyParser.json(), (req, res) =>{
+    products.registerProduct(req, res)
+})
+
+router.patch('/products/:skateID', bodyParser.json(), (req, res) =>{
+    products.updateProduct(req, res)
+})
+
+router.delete('/products/:skateID', (req, res) =>{
+    products.deleteProduct(req, res)
 })
 
 module.exports = {
--- api/model/Products.js
+++ api/model/Products.js
@@ -12,6 +12,59 @@ class Products{
             })
         })
     }
+
+    fetchProduct(req, res){
+        const query = `SELECT * FROM skateboards WHERE skateID = ?`
+        const { skateID } = req.params
+
+        db.query(query, [skateID], (err, result) =>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                result
+            })
+        })
+    }
+
+    registerProduct(req, res){
+        const query = `INSERT INTO skateboards SET ?`
+        const data = req.body
+
+        db.query(query, [data], (err) =>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "Product has been added"
+            })
+        })
+    }
+
+    updateProduct(req, res){
+        const query = `UPDATE skateboards SET ? WHERE skateID = ?`
+        const { skateID } = req.params
+        const data = req.body
+
+        db.query(query, [data, skateID], (err) =>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "Skateboard has been updated"
+            })
+        })
+    }
+
+    deleteProduct(req, res){
+        const query = `DELETE FROM skateboards WHERE skateID = ?`
+        const { skateID } = req.params
+
+        db.query(query, [skateID], (err, result) =>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "Skateboard has been deleted"
+            })
+        })
+    }
 }
 
 module.exports = Products
